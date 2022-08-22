7fd0a59fedca1d53c4895eb5c30816e805b082fe
rikardny <rikardnystrom95@gmail.com>
2022-08-22 11:46:14 +0200

    added sartorius xrandr profile

---
+++ arandr/sart.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output DP-1.1 --primary --mode 2560x1440 --pos 2560x0 --rotate normal --output DP-1.2 --mode 2560x1440 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --off
