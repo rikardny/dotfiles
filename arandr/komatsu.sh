0e0455dd34f1ea1931cea4efdd45a60a719a0d50
rikard <rikardnystrom95@gmail.com>
2024-12-09 11:16:53 +0100

    nvim opts

--- arandr/komatsu.sh
+++ arandr/komatsu.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 xrandr 	--output eDP-1 --primary --mode 1920x1200 --pos 3840x0 --rotate normal \
-	--output DP-1-3.1 --mode 1920x1200 --pos 1920x0 --rotate normal \
+	--output DP-1-3.1 --mode 1920x1200 --pos 1920x0 --rotate normal --primary \
 	--output DP-1-3.3 --mode 1920x1200 --pos 0x0 --rotate normal 
+i3-msg workspace 4
+i3-msg move workspace to output primary
+i3-msg workspace 1 
+i3-msg move workspace to output left
+i3-msg workspace 10
+i3-msg move workspace to output right
--- nvim/lua/custom/plugins/init.lua
+++ nvim/lua/custom/plugins/init.lua
@@ -2,5 +2,7 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
 return {}
