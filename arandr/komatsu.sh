53ee4f9e3e982a8a1218ecb2403ab68e910ac657
rikardny <rikardnystrom95@gmail.com>
2025-11-20 16:43:28 +0100

    updated gitignore

--- arandr/komatsu.sh
+++ arandr/komatsu.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 xrandr --output eDP-1 --primary --mode 1920x1200 --pos 0x240 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off --output HDMI-1 --off --output HDMI-2 --off --output DP-1-3.2.8 --mode 2560x1440 --pos 1920x0 --rotate normal --output DP-1-0 --off --output DP-1-1 --off --output HDMI-1-0 --off --output DP-1-2 --off --output DP-1-3 --off
+nitrogen --restore
+notify-send xrandr "Changed to Komatsu profile"
---
+++ nvim/lua/custom/plugins/vim-be-good.lua
@@ -0,0 +1,5 @@
+return {
+  'ThePrimeagen/vim-be-good',
+  version = '*', -- Use for stability; omit to use `main` branch for the latest features
+  event = 'VeryLazy',
+}
